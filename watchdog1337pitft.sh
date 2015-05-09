4ddc367343a45cb45f2f4a0902d44c9d0f1746d7
Christer Jonassen <christer.jonassen@gmail.com>
2015-05-09 21:42:22 +0200

    ConsoleX notify.sh support

--- watchdog1337pitft.sh
+++ watchdog1337pitft.sh
@@ -216,6 +216,12 @@ summarynext() #Displays a status summary and statistics and waits the number of 
 		echo -e "$RED""///"$YELLOW" SUMMARY @ $HMS: "$DEF""$LIGHTGRAY"$HOSTSOK"$DEF""$GRAY" of "$DEF""$LIGHTGRAY"$HOSTS"$DEF""$GRAY" hosts are "$LIGHTGREEN"UP"$DEF" "
 	else
 		echo -e "$RED""///"$YELLOW" SUMMARY @ $HMS: "$DEF""$LIGHTGRAY"$HOSTSDOWN"$DEF""$GRAY" of "$DEF""$LIGHTGRAY"$HOSTS"$DEF""$GRAY" hosts are "$LIGHTRED"DOWN"$DEF" "
+		if [[  -f  ~/container/notifysh/notify.sh ]]; then
+			echo "// WATCHDOG1337 //" >> ~/container/persistence/notifysh/notify.txt
+			echo "SUMMARY @ $NOWSTAMP:" >> ~/container/persistence/notifysh/notify.txt
+			echo "$HOSTSDOWN of $HOSTS hosts are DOWN " >> ~/container/persistence/notifysh/notify.txt
+			echo >> ~/container/persistence/notifysh/notify.txt
+		fi
 	fi
 	if [[ "$LOOP" = true  ]]; then
 		tput sc
